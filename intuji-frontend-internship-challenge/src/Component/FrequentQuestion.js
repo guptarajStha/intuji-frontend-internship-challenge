e5b3390f29e74887fe5da73d1655bacb1833e3ee
guptarajStha <shresthaguptaraj@gmail.com>
2024-05-28 19:15:07 +0545

    update Question section

--- intuji-frontend-internship-challenge/src/Component/FrequentQuestion.js
+++ intuji-frontend-internship-challenge/src/Component/FrequentQuestion.js
@@ -10,8 +10,8 @@ const FrequentQuestion = () => {
   return (
     <div className="py-[80px] relative">
       <h1 className="text-[71px] font-extrabold w-[48%]">Frequently asked questions</h1>
-      <div className="w-full flex justify-between py-10 px-2">
-        <div className="w-[65%] flex flex-col gap-4">
+      <div className="w-full flex flex-col lg:flex-row justify-between py-10 px-2">
+        <div className="lg:w-[65%] flex flex-col gap-4">
           {question.map((value, index) => {
             return (
               <div
@@ -29,8 +29,8 @@ const FrequentQuestion = () => {
             );
           })}
         </div>
-        <div className="w-[30%] bg-white border-[1px] border-[#CFCFCF] rounded-[10px] p-6 flex flex-col justify-between">
-          <div className="flex flex-col justify-center items-center text-center  gap-4 mt-8">
+        <div className=" mt-4 lg:mt-0 gap-4 lg:gap-0 lg:w-[30%] bg-white border-[1px] border-[#CFCFCF] rounded-[10px] p-6 flex flex-col justify-between">
+          <div className="flex lg:flex-col justify-center items-center text-center  gap-4 mt-8">
             <img src={msg} alt="" />
             <h1 className="font-extrabold text-[22px]">Do you have more questions?</h1>
             <h2 className="text-[16px] font-semibold">
